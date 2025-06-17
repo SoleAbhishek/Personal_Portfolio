a80922b3f54c56e843ac349658100529dd7f4144
Abhishek <abhisheksole1817@gmail.com>
2025-06-18 02:26:53 +0530

    Minor Change

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -5,7 +5,7 @@ import { HeroSection } from "@/Components/HeroSection";
 import { AboutSection } from "@/Components/AboutSection";
 import { SkillsSection } from "@/Components/SkillsSection";
 import { ProjectsSection } from "@/Components/ProjectsSection";
-import { ContactSection } from "@/components/ContactSection";
+import { ContactSection } from "@/Components/ContactSection";
 import { Footer } from "@/Components/Footer";
 
 export const Home = () => {
